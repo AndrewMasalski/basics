21e50e60465857349a5b697e797237a0a033e85c
Andrew Masalski <amasalski@exadel.com>
2017-02-07 18:20:39 +0300

    angluar phonebook sample

---
+++ angular/phone-book/app.js
@@ -0,0 +1,47 @@
+angular
+    .module('Phone', ['angularModalService', 'ngAnimate'])
+    .controller('ModalController', function($scope, close) {
+
+        $scope.close = function(result) {
+            close(result, 500); // close, but give 500ms for bootstrap to animate
+        };
+
+    })
+    .controller('PhoneCtrl', function($scope, ModalService) {
+        $scope.show = function() {
+            ModalService.showModal({
+                templateUrl: 'modal.html',
+                controller: "ModalController"
+            }).then(function(modal) {
+                modal.element.modal();
+                modal.close.then(function(result) {
+                    $scope.message = "You said " + result;
+                });
+            });
+        };
+
+        $scope.groups = ['Friends', 'Family', 'Spam', 'Favorites', 'NoGrouped', 'Work'];
+        $scope.contacts = [];
+        $scope.id = 0;
+        $scope.name = '';
+        $scope.phone = '';
+        $scope.email = '';
+        $scope.group = '';
+        $scope.selectedOption = '';
+        $scope.add = function() {
+            var q = confirm('Add?');
+            $scope.contacts.push({
+                id: $scope.id,
+                name: $scope.name,
+                phone: $scope.phone,
+                email: $scope.email,
+                group: $scope.selectedOption
+            });
+            $scope.id = 0;
+            $scope.name = '';
+            $scope.phone = '';
+            $scope.email = '';
+            $scope.group = '';
+            $scope.selectedOption = '';
+        };
+    });
