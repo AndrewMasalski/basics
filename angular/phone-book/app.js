fbb9d9a358eb6eed6157c7c173c9c74bdea062a8
Andrew Masalski <amasalski@exadel.com>
2017-02-15 16:39:33 +0300

    angluar phonebook pass inputs validation

--- angular/phone-book/app.js
+++ angular/phone-book/app.js
@@ -1,24 +1,31 @@
-angular
+var phonebook = angular
     .module('Phone', ['angularModalService', 'ngAnimate'])
-    .controller('ModalController', function($scope, close, name, phone) {
+    .controller('ModalController', function($scope, close, name, phone, email) {
         $scope.name = name;
         $scope.phone = phone;
+        $scope.email = email;
         $scope.close = function(result) {
             close(result, 500); // close, but give 500ms for bootstrap to animate
         };
+
     })
     .controller('PhoneCtrl', function($scope, ModalService) {
         $scope.show = function() {
-            ModalService.showModal({
-                templateUrl: 'modal.html',
-                controller: "ModalController",
-                inputs: {name: $scope.name, phone: $scope.phone}
-            }).then(function(modal) {
-                modal.element.modal();
-                modal.close.then(function(result) {
-                    $scope.message = "You said " + result;
+            if (!$scope.userForm.$valid) {
+                angular.forEach($scope.userForm.$error.required, function(field) { field.$setDirty(); });
+            } else {
+                ModalService.showModal({
+                    templateUrl: 'modal.html',
+                    controller: "ModalController",
+                    inputs: {name: $scope.name, phone: $scope.phone, email: $scope.email}
+                }).then(function(modal) {
+                    modal.element.modal();
+                    modal.close.then(function(result) {
+                        /*$scope.message = "You said " + result;*/
+                        if (result === 'Yes') {$scope.add()}
+                    });
                 });
-            });
+            }
         };
 
         $scope.groups = ['Friends', 'Family', 'Spam', 'Favorites', 'NoGrouped', 'Work'];
@@ -30,7 +37,6 @@ angular
         $scope.group = '';
         $scope.selectedOption = '';
         $scope.add = function() {
-            var q = confirm('Add?');
             $scope.contacts.push({
                 id: $scope.id,
                 name: $scope.name,
