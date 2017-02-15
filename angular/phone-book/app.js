264b9e4513e30f4881c33b50d6684f3f77ba3e83
Andrew Masalski <amasalski@exadel.com>
2017-02-15 18:26:50 +0300

    angluar phonebook persistent

--- angular/phone-book/app.js
+++ angular/phone-book/app.js
@@ -1,54 +1,81 @@
-var phonebook = angular
-    .module('Phone', ['angularModalService', 'ngAnimate'])
-    .controller('ModalController', function($scope, close, name, phone, email) {
-        $scope.name = name;
-        $scope.phone = phone;
-        $scope.email = email;
-        $scope.close = function(result) {
-            close(result, 500); // close, but give 500ms for bootstrap to animate
-        };
-
+angular
+    .module('PhoneBook', ['LocalStorageModule', 'angularModalService', 'ngAnimate'])
+    .config(function(localStorageServiceProvider) {
+        localStorageServiceProvider.setPrefix('PhoneBook').setNotify(true, true);
     })
-    .controller('PhoneCtrl', function($scope, ModalService) {
-        $scope.show = function() {
-            if (!$scope.userForm.$valid) {
-                angular.forEach($scope.userForm.$error.required, function(field) { field.$setDirty(); });
-            } else {
-                ModalService.showModal({
-                    templateUrl: 'modal.html',
-                    controller: "ModalController",
-                    inputs: {name: $scope.name, phone: $scope.phone, email: $scope.email}
-                }).then(function(modal) {
+    .controller('ContactDetailsController', function($scope, close, action, contact) {
+        $scope.groups = ['None', 'Friends', 'Family', 'Spam', 'Favorites', 'Work'];
+        $scope.contact = contact || {group: $scope.groups[0]};
+        $scope.action = action;
+        if ($scope.action === 'Delete') {
+            $scope.readOnly = true;
+        }
+        $scope.close = function(result) {
+            close(result, 333);
+        };
+    })
+    .controller('PhoneBookCtrl', function($scope, ModalService, localStorageService) {
+        $scope.contact = {};
+        $scope.contacts = localStorageService.get('contacts') || [];
+
+        function ask(action, data, callback) {
+            var modalOptions = {
+                templateUrl: 'contactDetails.html',
+                controller: "ContactDetailsController",
+                inputs: {
+                    action: action,
+                    contact: data
+                }
+            };
+            ModalService.showModal(modalOptions)
+                .then(function(modal) {
                     modal.element.modal();
                     modal.close.then(function(result) {
-                        /*$scope.message = "You said " + result;*/
-                        if (result === 'Yes') {$scope.add()}
+                        if (result) {
+                            callback(result)
+                        }
                     });
-                });
+                })
+        }
+
+        $scope.create = function() {
+            ask('Create', undefined, $scope.addToStorage);
+        };
+
+        $scope.edit = function(contact) {
+            var clone = angular.copy(contact);
+            ask('Edit', clone, $scope.saveToStorage);
+        };
+
+        $scope.delete = function(contact) {
+            ask('Delete', contact, $scope.deleteFromStorage);
+        };
+
+        $scope.addToStorage = function(contact) {
+            $scope.contacts.push({
+                id: new Date().getTime(),
+                name: contact.name,
+                phone: contact.phone,
+                email: contact.email,
+                group: contact.group
+            });
+            localStorageService.set('contacts', $scope.contacts);
+        };
+
+        $scope.saveToStorage = function(clone) {
+            for (var i = 0; i < $scope.contacts.length; i++) {
+                var contact = $scope.contacts[i];
+                if (contact.id == clone.id) {
+                    angular.merge(contact, clone);
+                    localStorageService.set('contacts', $scope.contacts);
+                    break;
+                }
             }
         };
 
-        $scope.groups = ['Friends', 'Family', 'Spam', 'Favorites', 'NoGrouped', 'Work'];
-        $scope.contacts = [];
-        $scope.id = 0;
-        $scope.name = '';
-        $scope.phone = '';
-        $scope.email = '';
-        $scope.group = '';
-        $scope.selectedOption = '';
-        $scope.add = function() {
-            $scope.contacts.push({
-                id: $scope.id,
-                name: $scope.name,
-                phone: $scope.phone,
-                email: $scope.email,
-                group: $scope.selectedOption
-            });
-            $scope.id = 0;
-            $scope.name = '';
-            $scope.phone = '';
-            $scope.email = '';
-            $scope.group = '';
-            $scope.selectedOption = '';
+        $scope.deleteFromStorage = function(contact) {
+            var index = $scope.contacts.indexOf(contact);
+            $scope.contacts.splice(index, 1);
+            localStorageService.set('contacts', $scope.contacts);
         };
     });
