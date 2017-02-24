eac46859cbb75d799035932a75985221fd54affd
Andrei.Masalski <amasalski@exadel.com>
2017-02-24 11:41:55 +0300

    made package.json app

--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var colors = require('colors');
 var app = express();
 
-var port = 3003;
-var host = 'http://localhost:' + port;
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
 
-app.use(require('./cors'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.listen(port, function() {
-    console.log('phone book server started: ' + (host + '/api').underline.green)
+app.listen(3003, function() {
+    console.log('phone book server started: http://localhost/api');
 });
 
 
