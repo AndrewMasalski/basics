c9dda5eefc168ec4a24f0e21ad62bdaa9bf9e68b
Andrei.Masalski <amasalski@exadel.com>
2017-02-24 11:39:30 +0300

    made package.json app

--- server/index.js
+++ server/index.js
@@ -7,20 +7,9 @@ var app = express();
 var port = 3003;
 var host = 'http://localhost:' + port;
 
-var router = express.Router();
-
-router.route('/')
-    .get(function(req, res) {
-        res.send({
-            contacts: host + '/api/contacts'
-        });
-    });
-
 app.use(require('./cors'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/api', api);
-app.use('/api', router);
 
 app.listen(port, function() {
     console.log('phone book server started: ' + (host + '/api').underline.green)
